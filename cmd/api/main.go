3ba64fed12dad2c18af03f12e9aa964479a1d817
Amal <amalb11164@gmail.com>
2024-04-03 17:26:05 +0530

    setup database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
 
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Dsn string `required:"true"`
+}
+
+const AppPrefix = "LAF"
+
+func main() {
+	var config Config
+	err := envconfig.Process(AppPrefix, &config)
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	db, err := openDB(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+}
+
+func openDB(config Config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", config.Dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping database: %v", err)
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/amal064/lostandfound-backend
 
 go 1.22.1
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/kelseyhightower/envconfig v1.4.0
+
+require github.com/lib/pq v1.10.9
