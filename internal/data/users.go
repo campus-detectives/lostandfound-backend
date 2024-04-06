53914e7e6640d6cd1f193b8f36c4a4c47d09bb7a
Amal <amalb11164@gmail.com>
2024-04-06 18:25:06 +0530

    Add users model

---
+++ internal/data/users.go
@@ -0,0 +1,160 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+
+	"github.com/amal064/lostandfound-backend/internal/validator"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrDuplicateUsername = errors.New("duplicate username")
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Username  string    `json:"username"`
+	Password  password  `json:"-"`
+	Version   int       `json:"-"`
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+func (p *password) Set(plaintext string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintext), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintext
+	p.hash = hash
+
+	return nil
+}
+
+func (p *password) Matches() (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(*p.plaintext))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
+
+}
+
+func ValidateUsername(v *validator.Validator, username string) {
+	v.Check(username != "", "username", "must be provided")
+	v.Check(len(username) >= 3, "username", "must be at least 3 characters long")
+	v.Check(len(username) <= 30, "username", "must not be more than 30 characters long")
+	v.Check(validator.Matches(username, validator.UsernameRX), "username", "must only contain letters, numbers, underscores, or dashes")
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 characters long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 characters long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) >= 3, "username", "must be at least 3 characters long")
+	v.Check(len(user.Name) <= 30, "name", "must not be more than 500 bytes long")
+
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
+}
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (m UserModel) Insert(user *User) error {
+	query := `
+	INSERT INTO users (name, username, password_hash) 
+	VALUES ($1, $2, $3)
+	RETURNING id, created_at, version`
+
+	args := []interface{}{user.Name, user.Username, user.Password.hash}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.CreatedAt, &user.Version)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateUsername
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func (m UserModel) GetByUsername(username string) (*User, error) {
+	query := `
+		SELECT id, created_at, name, username, password_hash, version 
+		FROM users
+		WHERE username = $1`
+
+	var user User
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, username).Scan(&user.ID,
+		&user.CreatedAt, &user.Name, &user.Username, &user.Password.hash, &user.Version,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &user, nil
+}
+
+func (m UserModel) Update(user *User) error {
+	query := `
+		UPDATE users
+		SET name = $1, username = $2, password_hash = $3, version = version + 1 
+		WHERE id = $4 AND version = $5
+		RETURNING version`
+
+	args := []interface{}{user.Name, user.Username, user.Password.hash, user.ID, user.Version}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.Version)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_username_key"`:
+			return ErrDuplicateUsername
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
+}
